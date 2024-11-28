083de7353fd1f81874a84dfb55f63e5908263e97
Guilherme lima <66636474+GuiDev182@users.noreply.github.com>
2024-11-27 22:21:21 -0300

    laço for

--- for-loop.py
+++ for-loop.py
@@ -37,14 +37,16 @@ print("Feliz 2025!!!")'''
 for c in range(0,51,2):
     print(c)'''
 
+
 #faça um programa que calcule a soma entre todos os numeros impares que são multiplos de tres
 # e que se encontram no intervalo de 1 ate 500
-
 '''soma = 0
-for multiplos in range(0+1,501,3):
-    soma += multiplos
-    print(multiplos)
-print(f"a soma total é de {soma}")'''
+cont = 0
+for multiplos in range(1,501,2):
+    if multiplos % 3 == 0:
+        cont += 1
+        soma += multiplos
+print(f"a soma de todos os valores é de {soma}")'''
 
 #Faça um programa que leia um numero inteiro qualquer e mostre na sua tela a sua tabuada
 
@@ -52,6 +54,50 @@ print(f"a soma total é de {soma}")'''
 
 for i in range(10):
      print(f"{numero} x {i+1} = {numero*(i+1)}")'''
+
+#desenvolva um programa que leia seis numeros inteiros e mostre a soma apenas daqueles que forem pares.
+#se o valor digitado for impar, desconsidere
+'''soma = 0
+cont = 0
+for c in range(1,7):
+    num = int(input(f"Digite o {c} numero: "))
+    if num % 2 == 0:
+        soma += num
+        cont += 1
+print(f"voce informou {cont} numeros pares e a soma foi {soma}")'''
+
+#desenvolva um programa que leia o primeiro termo e a razão de uma pa.
+#no final,mostre os 10 primeiros termos dessa progressao.
+
+
+'''primeiro_termo = int(input("Digite o primeiro termo de uma PA: "))
+razao = int(input("Digite a razão da PA: "))
+decimo = primeiro_termo + (11 - 1) * razao
+for c in range(primeiro_termo,decimo,razao):
+        print(c)'''
+
+
+#Faça um programa que leia um numero inteiro e diga se ele é ou nao um numero primo
+
+'''num = int(input("Digite um numero inteiro: "))
+tot = 0
+for c in range(1, num + 1):
+    if num % c == 0:
+        print('\033[34m', end='')
+        tot += 1
+    else:
+        print('\033[m', end='')
+    print(f'{c}')
+print(f'o numero {num} foi divisivel {tot}')
+if tot == 2:
+    print('e por isso ele é PRIMO!')
+else:
+    print('e por isso ele NÃO É PRIMO!')'''
+
+
+
+
+
 
 
 
