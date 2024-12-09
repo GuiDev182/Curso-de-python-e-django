2fab5837c14999a7fecdeec3d776084f2aeeb8fe
GuiDev182 <guilhermesemusa@gmail.com>
2024-12-08 22:07:09 -0300

    comando continue e pass

---
+++ continue-pass.py
@@ -0,0 +1,13 @@
+#continue
+for x in range(10):
+    if x == 3:
+        continue #laço for de 1 a 10, quando ele chegar no 3, ele vai pular o numero 3 e vai continuar o programa
+    print(x)
+
+#pass
+for x in range(10):
+    if x == 3:
+        pass #laço for de 1 a 10, quando ele chegar no 3, ele vai pular o numero 3 e vai continuar o programa, mas não imprime nada
+    print(x)
+
+#break
