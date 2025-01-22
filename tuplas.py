db7c4b098f05403795915d0f6f219348aa6c252b
Guilherme lima <66636474+GuiDev182@users.noreply.github.com>
2025-01-22 16:48:05 -0300

    update tuplas

--- tuplas.py
+++ tuplas.py
@@ -1,4 +1,4 @@
-'''lanche = ('hamburguer', 'suco', 'pizza')
+lanche = ('hamburguer', 'suco', 'pizza')
 print(sorted(lanche)) #ordena uma tupla/lista/dicionario
 #Tuplas são imutaveis
 
@@ -17,7 +17,7 @@ a = (2,5,4)
 b = (1,3,6)
 c = a + b
 print(c.count(5)) #mostra qyuantas vezes aparece o valor 5 na tupla
-print(c.index(8, 1)) # mostra em qual posição da tupla esta o valor 8, a partir do indice 1'''
+print(c.index(8, 1)) # mostra em qual posição da tupla esta o valor 8, a partir do indice 1
 from random import randint, Random
 
 #crie um programa que tenha uma tupla totalmente preenchida com uma contagem
@@ -25,7 +25,7 @@ from random import randint, Random
 #seu programa devera ler um numero pelo teclado(entre 0 e 20)
 # e mostralo por extenso
 
-'''contagem = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis','sete','oito','nove','dez',
+contagem = ('zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis','sete','oito','nove','dez',
             'onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte')
 
 numero = int(input("Digite um numero entre 0 e 20: \n R: "))
@@ -51,7 +51,7 @@ while numero > 0 or numero <= 20:
         break
     else:
         print("Numero invalido, tente novamente!")
-        numero = int(input("Digite um numero entre 0 e 20: \n R: "))'''
+        numero = int(input("Digite um numero entre 0 e 20: \n R: "))
 
 
 #crie uma tupla preenchida com os 20 primeiros colocados da tebala do campeonato brasileiro
@@ -61,7 +61,7 @@ while numero > 0 or numero <= 20:
 # C) uma lista com os times em ordem alfabetica
 # Em que posição na tabela está o time do cruzeiro
 
-'''times = ('Atletico-mg','bahia','botafogo','Ceará','corinthias',
+times = ('Atletico-mg','bahia','botafogo','Ceará','corinthias',
          'cruzeiro','flamengo','fluminense','fortaleza','gremio','internacional'
          ,'juventude','mirassol','palmeiras','red bull bragantino','santos','sao paulo','sport','vasco',
          'vitoria')
@@ -69,12 +69,45 @@ while numero > 0 or numero <= 20:
 print(f"os primeiros colocados são {times[0:5]}")
 print(f"os 4 ultimos colocados são {times[-4:]}")
 print(f"lista dos times em ordem alfabetica {sorted(times)}")
-print(f"o time do cruzeiro esta na {times.index('cruzeiro')} posição")'''
+print(f"o time do cruzeiro esta na {times.index('cruzeiro')} posição")
 
 #crie um programa que vai gerar cinco numeros aleatorios e colocar em uma tupla
 #depois disso,mostre a listagem de numeros gerados e tambem indique o menor
 #e o maios valor que estao na tupla
-tupla = ()
-numeros_aleatorios = Random.random()
+from random import Random
 
-print(numeros_aleatorios)
+tupla = ()
+
+while len(tupla) < 5:
+    tupla += (Random().randint(1,10 ),)
+    
+print(f'Os valores sorteados foram {tupla}')
+print(f'O maior valor sorteado foi {max(tupla)}')
+print(f'O menor valor sorteado foi {min(tupla)}')
+
+#desenvolva um programa que leia  4 valores pelo teclado e guarde em um tupla. No final mostra:
+#quantas vezes apareceu o valor 9 
+#em que posição foi digitado o primeiro valor 3
+# quais foram os numeros pares
+
+tupla = ()
+numeros_pares = ()
+tupla = input("Digite um numero: ")
+
+while len(tupla) < 4:
+    tupla += (input("digite um numero: "))
+if '9' in tupla:
+    print(f"o numero 9 aparece {tupla.count('9')} vez na tupla")
+if '3' in tupla:
+    print(f"o numero 3 aparece na posição {tupla.index('3',0)} pela primeira vez ")
+
+if int(tupla[0]) % 2 == 0:
+    numeros_pares = tupla[0]
+elif int(tupla[1]) % 2 == 0:
+    numeros_pares = tupla[1]
+elif int(tupla[2]) % 2 == 0:
+    numeros_pares = tupla[2]
+elif int(tupla[3]) % 2 == 0:
+    numeros_pares = tupla[3]
+print(f"os numeros pares são {numeros_pares}")
+
