c0c3640223269dbc3cd74928f8a9e45819850019
Guilherme lima <66636474+GuiDev182@users.noreply.github.com>
2025-01-27 19:45:00 -0300

    update exercicios de tuplas e listas

--- input-e-listas.py
+++ input-e-listas.py
@@ -1,4 +1,4 @@
-email = 'guilherme_email@gmail.com'
+'''email = 'guilherme_email@gmail.com'
 
 novo_email = email.replace('gmail','hotmail')
 
@@ -86,6 +86,68 @@ lista.sort() # ordena de forma crescente
 lista.sort(reverse=True) # ordena de forma decrescente
 
 print(lista)
+
+
+#adicionar um elemento em uma posição especifica
+
+lista = [1,2,3,4,5,6,7,8,9,10]
+lista.insert(0,0)
+print(lista)
+
+#remover um elemento de uma posição especifica
+
+lista = [1,2,3,4,5,6,7,8,9,10]
+del lista[0]
+lista.pop(0) ou lista.pop() #remove o ultimo elemento da lista ou da posição 0
+lista.remove(1)
+
+#criar lista a partit de um range
+
+lista = list(range(0,10))
+
+print(lista)
+
+valores = list()
+valores.append(1)
+valores.append(2)
+valores.append(3)
+
+for c, v in enumerate(valores):
+    print(f'na posição {c} encontrei o valor {v}')'''
+
+#faça uma programa que leia 5 valores numericos e guarde em uma lista.
+#no final mostre qual foi o maior  e o menor valor digitado e as suas
+#respectivas posições na lista
+
+'''valores_numericos = []
+
+for valores in range(0,5):
+    valores_numericos.append(int(input("Digite um valor: ")))
+for posicao, valor in enumerate(valores_numericos):
+    print(f"na posição {posicao} encontrei o numero {valor}")
+
+print(f"o menor valor foi {min(valores_numericos)}")
+print(f"o maior valor foi {max(valores_numericos)}")'''
+
+#crie um programa onde o usuario possa digitar varios valores numericos
+# e cadastre-os e uma lista. Caso o numero já exista lá dentro, ele não
+# será adicionado. No final, serão exibidos todos os valores únicos
+#digitados, em ordem crescente.
+
+valores_numericos = []
+valores_digitados = enumerate(valores_numericos[-1])
+
+valores_numericos.append(int(input("Digite um valor: ")))
+menu = int(input("Deseja continuar a digitar: \n 1 - Sim \n 2 - Não"))
+
+while menu != 2:
+    valores_numericos.append(int(input("Digite um valor: ")))
+    menu = int(input("Deseja continuar a digitar: \n 1 - Sim \n 2 - Não"))
+    if valores_digitados in valores_numericos:
+        print("esse valor já esta na lista")
+        valores_numericos.pop()
+print(f"os valores em ordem crescente é: {valores_numericos.sort()}")
+
 
 
 
